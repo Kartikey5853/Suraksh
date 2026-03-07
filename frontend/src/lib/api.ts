64a7d29a191bb929c807540381c21eb07fcf5726
kartikey5853 <kartikeymanthra@gmail.com>
2026-03-07 09:17:02 +0530

    frontend url changed

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://suraksh-4ezn.onrender.com";
 
 export const api = axios.create({
   baseURL: BASE_URL,
